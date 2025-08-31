a2601d8152e0aabaf2f82dcc40e3d1049600bd59
lia-005 <dipannitachowdhury81@gmail.com>
2025-09-01 01:19:18 +0530

    add files & update README.MD

---
+++ Day01/operation.js
@@ -0,0 +1,23 @@
+console.log(2+3);
+console.log(2-3);
+console.log(2*3);
+console.log(2/3);
+console.log(2%3);
+console.log(2**3); // exponentiation operator, 2 raised to the power of 3
+console.log("2"+3+3)// string concatenation, results in "233"
+console.log("2"*3); // string "2" is converted to number 2, result is 6
+console.log("2"-3+4); // string "2" is converted to number 2, result is -1
+console.log(3+5+"8")//in case of + operator 1st rhe number is added and then the string is concatenated, result is "88"
+console.log((3*5)+9-(85/5))//used brackets to change the order of operations, BODMAS rules apply 
+console.log(+ true); // converts boolean true to number, result is 1
+console.log(+ false); // converts boolean false to number, result is 0
+console.log(+""); // converts empty string to number, result is 0
+let a="DIPANNITA"
+let b=" CHOWDHURY"
+c=console.log(a+b)
+v=5
+console.log(v++);// post-increment operator, prints 5 and then increments v to 6
+console.log(++v); // pre-increment operator, increments v to 7 and then prints 7
+console.log(v--); // post-decrement operator, prints 7 and then decrements v to 6
+//check the notes 
+
