bedf0480b3cdaaf2bf7ed006d35a51e418cd7c29
lia-005 <dipannitachowdhury81@gmail.com>
2025-09-07 14:48:10 +0530

    datatypes added

---
+++ Day05/datatype.js
@@ -0,0 +1,18 @@
+"use strict";
+//alert()//it is for testing purposes only run in browser not in node js
+//we have to amke hogh readiblity code 
+console.log("Hello javascript!!"); log(6+2)
+//it is a low readibility code
+//datatypes
+let a = 10; //number range of 2^53-1
+let b = 10.5; //float
+let c = "Hello"; //string
+let d = true; //boolean it helps in decision making if the user is login or not
+let e = null; //null , its a special value that represents no value or empty value(stand alone)
+let f; //undefined, it is a variable that has been declared but not assigned a value
+let g = Symbol("unique"); //symbol, it is a unique value that can be used as an identifier 
+let h = BigInt(1234567890123456789012345678901234567890); //bigint, it is a large integer value that can be used to represent large numbers
+//object
+console.log(typeof a);// integer
+console.log(typeof undefined);// undefined ot is a type of datatype
+console.log(typeof null);// object, this is a bug in javascript it means its a own object
