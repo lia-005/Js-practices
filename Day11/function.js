2da1f25b7da2db5bb1d7a74abaaa5cecf01d495a
lia-005 <dipannitachowdhury81@gmail.com>
2025-09-19 08:57:21 +0530

    functions

---
+++ Day11/function.js
@@ -0,0 +1,8 @@
+function by(char1, char2) {
+  return console.log("Hello " + char1 + "  " + char2 + "!");
+}
+
+a = "Dipannita";
+b = "chowdhury";
+let c = by(a, b); // calling function
+console.log(c);
